46104f30bd3a26fac4e8d3097d084c79b2027c12
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2021-07-16 15:32:41 -0300

    Inserir usuarios

--- sistema.c
+++ sistema.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define MAX_NOME 30
 #define BANCO_DE_DADOS "pessoas.txt"
 
@@ -7,8 +8,9 @@ void inserir_registros(){
 
 	char nome[MAX_NOME];
 	FILE *fp;
+	char salvar='n';
 
-	fp = fopen(BANCO_DE_DADOS,"w");
+	fp = fopen(BANCO_DE_DADOS,"a");
 	if(fp==NULL){
 		fprintf(stderr,"%s: Falha ao abrir o arquivo %s\n",__FILE__,BANCO_DE_DADOS);
 		exit(1);
@@ -17,8 +19,24 @@ void inserir_registros(){
 	printf("Inserir novo registro na Agenda\n");
 	printf("Digite o nome do usuário:\n");
 	scanf(" %[^\n]",nome);
+	if((strlen(nome))<=2){
+		fprintf(stderr,"%s: Erro: O nome deve possuir mais de 2 caracteres!\n",__FILE__);
+		return;
+	}
 
-	fseek(fp,0,SEEK_END);
-	fprintf(fp,"%s\n",nome);
+	printf("O usuário a ser cadastrado:\n");
+	printf("Nome: %s\n",nome);
+	printf("Salvar (y/n)? "); scanf(" %c",&salvar);
+	if(salvar=='y'){
+		fseek(fp,0*sizeof(char),SEEK_END);
+		fprintf(fp,"%s\n",nome);
+		printf("Registro salvo com sucesso!\n");
+	}else if(salvar=='n'){
+		printf("Registro ignorado!\n");
+	}else{
+		fprintf(stderr,"%s: Erro: Opção inválida! (y/n)?",__FILE__);
+		return;
+	}
+
 	fclose(fp);
 }
